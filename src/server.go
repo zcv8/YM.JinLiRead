b56ca705f4ea9ac041bab36d40e5215430899a70
非白即黑 <3269985643@qq.com>
2018-01-29 21:49:04 +0800

    添加登录注册页面的模型

--- src/server.go
+++ src/server.go
@@ -1,9 +1,27 @@
 package main
 
-import(
-   "fmt"
+import (
+	"html/template"
+	"net/http"
 )
 
-func main(){
-  fmt.Println("Hello,World!")
+func main() {
+	mux := http.NewServeMux()
+	staticFile := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", staticFile))
+	mux.HandleFunc("/", loginHandler)
+
+	sever := &http.Server{
+		Addr:    "0.0.0.0:8011",
+		Handler: mux,
+	}
+	sever.ListenAndServe()
+}
+
+func loginHandler(wr http.ResponseWriter, r *http.Request) {
+	templates := []string{
+		"static/templates/login.html",
+	}
+	temps := template.Must(template.ParseFiles(templates...))
+	temps.ExecuteTemplate(wr, "login", "")
 }
