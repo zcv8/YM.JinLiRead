3f12c395e3be1445473df9af713135ff65c5d874
wangdi <wangdi@tidebuy.net>
2018-02-27 10:52:00 +0800

    重新添加文件

---
+++ common/session.go
@@ -0,0 +1,88 @@
+package common
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+)
+
+//用户登录的CookieName
+const AuthorizationKey = "AUTHORIZATIONKEY"
+
+//Session管理器
+type SessionManager struct {
+	cookieName  string
+	lock        sync.Mutex
+	provider    Provider
+	maxLifeTime int64
+}
+
+var provides = make(map[string]Provider)
+
+//注册Provider
+func Register(name string, provider Provider) {
+	if _, dup := provides[name]; dup {
+		panic("session: Register called twice for provider " + name)
+	}
+	provides[name] = provider
+}
+
+//创建Session管理器
+func NewManager(providerName string, cookieName string, maxLifeTime int64) (*SessionManager, error) {
+	provider, ok := provides[providerName]
+	if !ok {
+		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", providerName)
+	}
+	return &SessionManager{provider: provider, cookieName: cookieName, maxLifeTime: maxLifeTime}, nil
+}
+
+//根据每次用户访问发现与之关联的Session,没有关联的Session则创建
+func (manager *SessionManager) SessionStart(w http.ResponseWriter, r *http.Request, saveTime int64) (session Session) {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	cookie, err := r.Cookie(manager.cookieName)
+	//没发现cookie或者是cookie的值为空，则创建Session
+	if err != nil || cookie.Value == "" {
+		sid := GetGuid()
+		session, _ = manager.provider.SessionInit(sid, saveTime)
+		cookie = &http.Cookie{
+			Name:     AuthorizationKey,
+			Value:    url.QueryEscape(sid),
+			Path:     "/",
+			HttpOnly: true,
+			MaxAge:   int(manager.maxLifeTime),
+		}
+		http.SetCookie(w, cookie)
+
+	} else {
+		sid, _ := url.QueryUnescape(cookie.Value)
+		session, _ = manager.provider.SessionRead(sid, saveTime)
+	}
+	return
+}
+
+//销毁Session
+func (manager *SessionManager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(AuthorizationKey)
+	if err == nil && cookie.Value != "" {
+		manager.lock.Lock()
+		defer manager.lock.Unlock()
+		sid, _ := url.QueryUnescape(cookie.Value)
+		manager.provider.SessionDestroy(sid)
+		expiration := time.Now()
+		//销毁Session之后需要将浏览器的Cookie设置为立即过期，也就是过期时间设置为过去的一个时间
+		cookie = &http.Cookie{Name: manager.cookieName, Path: "/", HttpOnly: true, Expires: expiration, MaxAge: -1}
+	}
+	return
+}
+
+//对过期的Session进行垃圾回收
+func (manager *SessionManager) GC() {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	manager.provider.SessionGC(manager.maxLifeTime)
+	//每隔 maxLifeTime 的时间再次进行回收
+	time.AfterFunc(time.Duration(manager.maxLifeTime), func() { manager.GC() })
+}
