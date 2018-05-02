8a2108353425a7b7ef3982ec8fc03bd6084ce385
非白即黑 <3269985643@qq.com>
2018-05-02 22:13:20 +0800

    使用xorm目前存在bug 有待后续完善

--- data/data.go
+++ data/data.go
@@ -1,17 +1,18 @@
 package data
 
 import (
-	"database/sql"
-	_ "github.com/lib/pq"
 	"log"
+
+	"github.com/go-xorm/xorm"
+	_ "github.com/lib/pq"
 )
 
-var Db *sql.DB
+var Db *xorm.Engine
 
 //这个函数只会加载一次
 func init() {
 	var err error
-	Db, err = sql.Open("postgres", "host=47.98.40.152 port=5432 user=postgres password=123456 dbname=JinLiRead sslmode=disable")
+	Db, err = xorm.NewEngine("postgres", "host=47.98.40.152 port=5432 user=postgres password=123456 dbname=JinLiRead sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
