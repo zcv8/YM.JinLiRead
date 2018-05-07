f276e0f91757e702a19e649a7edea2f66fe63d6c
非白即黑 <3269985643@qq.com>
2018-05-07 20:43:27 +0800

    修改部分逻辑

---
+++ Entities/ArticleEntity.go
@@ -0,0 +1,29 @@
+package entities
+
+import "time"
+
+type Article struct {
+	ID         int       `json:"id"`
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	ChannelId  int       `json:"channel"`
+	Labels     string    `json:"labels"`
+	Type       int       `json:"type"`
+	Status     int       `json:"status"`
+	FirstImage string    `json:"image"`
+	CreateUser int       `json:"user"`
+	ReadCount  int       `json:"readcount"`
+	CreateTime time.Time `json:"createtime" xorm:"created"`
+	UpdateTime time.Time `json:"updatetime" xorm:"updated"`
+}
+
+//级联查询返回的结构体
+type ArticleInfo struct {
+	UserAdmin `xorm:"extends"`
+	Channel   `xorm:"extends"`
+	Article   `xorm:"extends"`
+}
+
+func (a *ArticleInfo) TableName() string {
+	return "Article"
+}
