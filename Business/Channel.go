78be16c1cf56fad50416eee031a2ff17c8a14be3
非白即黑 <3269985643@qq.com>
2018-05-07 21:09:43 +0800

    修复文件

---
+++ Business/Channel.go
@@ -0,0 +1,31 @@
+package business
+
+/*
+ * 处理与频道标签有关的业务逻辑
+ */
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/zcv8/YM.JinLiRead/common"
+	"github.com/zcv8/YM.JinLiRead/data"
+	entity "github.com/zcv8/YM.JinLiRead/entities"
+)
+
+// 获取所有频道标签
+func GetChannels(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	channels, err := data.GetChannels()
+	if err != nil {
+		json.NewEncoder(w).Encode(&entity.ResponseStatus{
+			Status:  entity.FAILED,
+			Message: common.ApplicationInternalError.SetOrginalError(err).Error(),
+		})
+		return
+	}
+	json.NewEncoder(w).Encode(&entity.ResponseStatus{
+		Status: entity.SUCCEED,
+		Data:   channels,
+	})
+}
---
+++ Business/Decorators.go
@@ -0,0 +1,35 @@
+package business
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/zcv8/YM.JinLiRead/common"
+	entity "github.com/zcv8/YM.JinLiRead/entities"
+)
+
+//身份识别验证器
+func Authentication(h httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		cookie, err := r.Cookie(common.AuthorizationKey)
+		isPass := false
+		if err == nil && cookie.Value != "" {
+			session, _ := Manager.SessionRead(w, r)
+			if session != nil {
+				isPass = true
+			}
+		}
+		if isPass {
+			h(w, r, ps)
+		} else {
+			rtr, _ := json.Marshal(&entity.ResponseStatus{
+				Status:  entity.FAILED,
+				Data:    nil,
+				Message: common.InterfaceUsageError.SetText("INVALID_SESSION").Error(),
+			})
+			fmt.Fprint(w, string(rtr))
+		}
+	}
+}
